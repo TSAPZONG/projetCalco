e80196370e7036dc30224dd1caf72a432cdd1f5e
stevia <ornelatsapzong@gmail.com>
2022-05-30 06:00:48 +0100

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+
+  public operation = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  effacer(): void {
+    this.operation = '';
+  }
+
+  effacer1(): void {
+
+    if (this.operation === 'ERREURE DE SYNTAXE') {
+      this.operation = '';
+    }
+
+    try {
+      // eslint-disable-next-line no-eval
+      this.operation = this.operation.slice(0, -1);
+    } catch {
+      this.operation = '';
+    }
+  }
+
+  egale(): void {
+    try {
+
+      // eslint-disable-next-line no-eval
+      this.operation = eval(this.operation);
+    } catch {
+      this.operation = 'ERREURE DE SYNTAXE';
+    }
+  }
+
+  entrer(n: any): void {
+    this.operation = this.operation + '' +n;
+  }
+
+}
